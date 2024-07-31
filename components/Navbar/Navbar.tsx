efcaa6a8c8074dac092cd26b43057f7943d90a8b
GabrielNavas98 <cgnavas98@gmail.com>
2024-07-31 14:21:50 -0300

    back button

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,20 +1,44 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter, usePathname } from 'next/navigation';
+import BackIcon from '@/icons/BackIcon';
 import logo from '@/public/images/logo.png';
-import './navbarStyles.css';
 
 const Navbar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const isRoot = pathname === '/';
+
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-top">
+    <nav className="flex flex-col border border-gray-300 bg-white">
+      <div className="flex items-start p-2.5 sm:p-4 border-b border-gray-300">
         <Image src={logo} alt="Logo esto es" width={50} height={25} />
       </div>
-      <div className="navbar-bottom">
-        <Link href="/" passHref>
-          <h2>My projects</h2>
-        </Link>
-        <Link href="/projectForm/new" passHref>
-          <button className="add-project-button">+ Add project</button>
+      <div className="flex justify-between items-center p-2.5 sm:p-4 text-lg font-semibold">
+        <div className="flex items-center">
+          {!isRoot && (
+            <button
+              onClick={handleBackClick}
+              className="text-gray-700 hover:text-gray-900 mr-4"
+            >
+              <BackIcon />
+            </button>
+          )}
+          <Link href="/" passHref className="text-gray-900">
+            My projects
+          </Link>
+        </div>
+        <Link
+          href="/projectForm/new"
+          passHref
+          className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700"
+        >
+          + Add project
         </Link>
       </div>
     </nav>
